7b940298bbd7b36b583ad07bb4d118b65e42f724
mert metin <erdemlimertmetin@gmail.com>
2025-03-19 20:17:37 +0300

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Merhaba, Flask Uygulamasına Hoş Geldiniz!"
+    return "Merhaba, Flask Uygulamasına Hoş Geldiniz MERTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT!"
 
 
 
