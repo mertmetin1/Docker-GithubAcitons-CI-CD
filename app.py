a03780343bf5cf12d71699c5e0987c357d7ec555
Mert Metin <98667673+mertmetin1@users.noreply.github.com>
2025-04-02 01:33:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Merhaba, Flask Uygulamasına Hoş Geldiniz MERTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT!"
-
-
-
-
-
-
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'yivli_insaat_logo_page-0001.jpg')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
