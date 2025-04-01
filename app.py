7d1e3b923409b5da6fa27882cefa7eea37331c17
Mert Metin <98667673+mertmetin1@users.noreply.github.com>
2025-04-02 01:39:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Merhaba, Flask Uygulamasına Hoş Geldiniz MERTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT!"
-
-
-
-
-
-
+    return '''
+    <h1>Merhaba, Flask Uygulamasına Hoş Geldiniz!</h1>
+    <img src="/static/yivli_insaat_logo_page-0001.jpg" alt="Image">
+    '''
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
