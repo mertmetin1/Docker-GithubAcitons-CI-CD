693c1b56584cdd1a53d5c08361501ce8c5232a66
Mert Metin <98667673+mertmetin1@users.noreply.github.com>
2025-04-02 01:54:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
@@ -6,8 +7,15 @@ app = Flask(__name__)
 def index():
     return '''
     <h1>Merhaba, Flask Uygulamasına Hoş Geldiniz!</h1>
-    <img src="yivli_insaat_logo_page-0001.jpg" alt="Image">
+    <iframe src="/pdf" width="100%" height="600px">
+        PDF dosyasını görüntüleyemiyorsanız, <a href="/pdf">buraya tıklayın</a>.
+    </iframe>
     '''
+
+@app.route('/pdf')
+def show_pdf():
+    # PDF dosyasının tam yolunu belirtiyoruz
+    return send_file('/app/yivliinsaat.pdf', as_attachment=False)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
